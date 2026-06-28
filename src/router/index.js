a9a336af4d562d70699b8c18c4b15782c622d71b
kauabarros-24 <martinsbarroskaua@gmail.com>
2026-06-27 21:54:42 -0300

    CHORE: New project

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import FeedView from '@/views/FeedView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: "/feed",
+      name: "home",
+      component: FeedView
+
+    }
+  ],
 })
 
 export default router
